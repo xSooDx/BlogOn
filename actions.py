251e36746990e33f117bdaef4d89ee6ff2c27c58
Sahil Shah <shahsahil007@gmail.com>
2017-04-09 22:41:00 +0530

    Updated from v2.1

--- actions.py
+++ actions.py
@@ -4,7 +4,9 @@ from flask import session
 from passlib.hash import sha256_crypt
 from BlogOnDB import *
 import gc
-from flask import jsonify
+
+from blogon_events import logEvent
+
 
 def close(c, conn):
     c.close()
@@ -40,7 +42,7 @@ def get_child_categories(category):
 
 def get_top_level_categories():
     c, conn = connect()
-    c.execute('SELECT * FROM categories WHERE parent=NULL');
+    c.execute('SELECT * FROM categories WHERE parent=NULL')
     categories = c.fetchall()
     return categories
 
@@ -56,15 +58,17 @@ def get_comments_by_post(postid):
     data = c.fetchall()
     comments = []
     for comm in data:
-        comments.append({'name':comm['name'],'com':comm['comment']})
-	#comments = [i['category'] for i in cats]
-    return {"comments":comments}
-    
-def post_comment(name, email,comment, postid):
+        comments.append({'name': comm['name'], 'com': comm['comment']})
+    # comments = [i['category'] for i in cats]
+    return {"comments": comments}
+
+
+def post_comment(name, email, comment, postid):
     c, conn = connect()
-    c.execute("INSERT INTO comments (postid, name,email,comment) values (%s,%s,%s,%s)", (postid,name, email, comment))
+    c.execute("INSERT INTO comments (postid, name,email,comment) values (%s,%s,%s,%s)", (postid, name, email, comment))
     conn.commit()
     close(c, conn)
+
 
 def get_comments_by_email(email):
     pass
@@ -80,11 +84,22 @@ def get_comment_by_id(id):
 
 # USERS
 def get_users():
-    pass
+    c, conn = connect()
+    c.execute("SELECT userid,username,email,rank FROM post_category")
+    d = c.fetchall()
+    users = []
+    for u in d:
+        users.append(User(u['userid'], u['username'], u['email'], u['rank']))
+    close(c, conn)
+    return users
 
 
-def get_users_by_id():
-    pass
+def get_user_by_id(userid):
+    c, conn = connect()
+    c.execute("SELECT userid,username,email,rank FROM users WHERE userid=%s", (int(userid),))
+    d = c.fetchone()
+    close(c, conn)
+    return d
 
 
 def get_post_categories(postid):
@@ -92,6 +107,7 @@ def get_post_categories(postid):
     c.execute("SELECT category FROM post_category WHERE postid=%s", (int(postid),))
     cats = c.fetchall()
     cats = [i['category'] for i in cats]
+    close(c, conn)
     return cats
 
 
@@ -99,77 +115,82 @@ def get_post_categories(postid):
 def get_num_posts():
     c, conn = connect()
     c.execute("SELECT count(*) as n FROM posts")
-    return c.fetchone()['n']
+    a = c.fetchone()['n']
+    close(c, conn)
+    return a
 
 
 def get_posts():
     c, conn = connect()
-    c.execute("SELECT * FROM posts")
+    c.execute("SELECT * FROM posts ORDER BY creation_date DESC ")
     data = c.fetchall()
-    posts = []
+
     for post in data:
-        c.execute("SELECT category FROM post_category WHERE postid=%s", (int(post['postid']),))
+        c.execute("SELECT category FROM post_category WHERE postid=%s ", (int(post['postid']),))
         cats = get_post_categories(post['postid'])
-        posts.append(Post(post['postid'], post['userid'], post['title'], post['content'], post['description'], cats,
-                          post['tags']))
+        post['categories'] = cats
     close(c, conn)
-    return posts
+    return data
 
 
 def get_post_by_id(postid):
     c, conn = connect()
-    c.execute("SELECT * FROM posts WHERE postid=%s", (int(postid),))
+    c.execute("SELECT * FROM posts WHERE postid=%s ORDER BY creation_date", (int(postid),))
     data = c.fetchone()
     c.execute("SELECT category FROM post_category WHERE postid=%s", (int(postid),))
     cats = get_post_categories(postid)
     close(c, conn)
-    return Post(data['postid'], data['userid'], data['title'],
-                data['content'], data['description'], data['tags'], cats)
+    data['categories'] = cats
+    return data
+
 
 def get_post_by_title(title):
-    str = " ".join(title.split("-"))
+    tstr = " ".join(title.split("-"))
     c, conn = connect()
-    c.execute("SELECT * FROM posts WHERE title=%s", (str,))
+    c.execute("SELECT * FROM posts WHERE title=%s", (tstr,))
     data = c.fetchone()
     c.execute("SELECT category FROM post_category WHERE postid=%s", (int(data['postid']),))
     cats = get_post_categories(data['postid'])
     close(c, conn)
-    return Post(data['postid'], data['userid'], data['title'],
-                data['content'], data['description'], data['tags'], cats)
+    data['categories']=cats
+    return data
+
 
 def get_posts_by_category(category):
     posts = []
     c, conn = connect()
-    c.execute("SELECT postid FROM post_category where category=%s", (thwart(category),))
+    c.execute("SELECT postid FROM post_category where category=%s creation_date", (thwart(category),))
     data = c.fetchall()
     data = [i['postid'] for i in data]
     for i in data:
         c.execute("SELECT * FROM posts WHERE postid=%s", (int(i),))
         post = c.fetchone()
         cats = get_post_categories(i)
-        posts.append(Post(post['postid'], post['userid'], post['title'], post['content'], post['description'], cats,
-                          post['tags']))
+        post['categories'] = cats
+        posts.append(post)
     close(c, conn)
     return posts
 
 
 def get_posts_by_user(user):  # name or authorid
     c, conn = connect()
-    posts = []
+
     if isinstance(user, str):
-        c.execute("SELECT * FROM posts WHERE username=%s", (thwart(user),))
+        c.execute("SELECT * FROM posts WHERE username=%s date_created", (thwart(user),))
         data = c.fetchall()
         for i in data:
-            posts.append(Post(i['postid'], i['userid'], i['title'], i['content'], i['description'],
-                              get_post_categories(i['postid']), i['tags']))
+            i['categories'] = get_post_categories(i['postid'])
 
     elif isinstance(user, int):
         c.execute("SELECT * FROM posts WHERE userid=%s", (int(user),))
         data = c.fetchall()
         for i in data:
-            posts.append(Post(i['postid'], i['userid'], i['title'], i['content'], i['description'],
-                              get_post_categories(i['postid']), i['tags']))
-    return posts
+            i['categories'] = get_post_categories(i['postid'])
+    else:
+        return -1
+    close(c, conn)
+    return data
+
 
 def content_functions():
     funcs = {
@@ -185,7 +206,7 @@ def content_functions():
         'get_comments_by_name': get_comments_by_name,
         'get_comments_by_email': get_comments_by_email,
         'get_users': get_users,
-        'get_users_by_id': get_users_by_id
+        'get_user_by_id': get_user_by_id
     }
     return funcs
 
@@ -222,6 +243,7 @@ def create_post(**d):
 
     conn.commit()
     close(c, conn)
+    logEvent("post create", "userid=" + str(session['userid']) + " postid=" + str(postid))
     return postid
 
 
@@ -268,6 +290,8 @@ def update_post(postid, **d):
 
     conn.commit()
     close(c, conn)
+    logEvent("post update", "userid=" + str(session['userid']) + " postid=" + str(postid))
+    return postid
 
 
 def publish_post(postid):
@@ -275,6 +299,8 @@ def publish_post(postid):
     c.execute("UPDATE posts SET published=1 WHERE postid=%s", (int(postid),))
     conn.commit()
     close(c, conn)
+    logEvent("post publish", "userid=" + str(session['userid']) + " postid=" + str(postid))
+    return postid
 
 
 def delete_post(postid):
@@ -282,6 +308,8 @@ def delete_post(postid):
     c.execute("DELETE FROM posts WHERE userid=%s AND postid=%s", (int(session['userid']), int(postid)))
     conn.commit()
     close(c, conn)
+    logEvent("post delete", "userid=" + str(session['userid']) + " postid=" + str(postid))
+    return postid
 
 
 def login(username, password, remember=False):
@@ -317,7 +345,8 @@ def register_user(username, email, password):
     userid = c.fetchone()['userid']
     set_login_session(userid, username, email)
     close(c, conn)
-    return 1
+    logEvent("user register", "userid=" + userid)
+    return userid
 
 
 def set_login_session(userid, username, email):
--- blogon_tasks.py
+++ blogon_tasks.py
@@ -1,8 +1,10 @@
 from flask import flash, jsonify
-from flask import request
+from flask import request, redirect
+from flask import url_for
+
 from main import app
 from blogon_views import blogon_login_required
-from actions import create_post, update_post, publish_post
+from actions import create_post, update_post, publish_post, delete_post
 
 
 @app.route("/blogon/tasks/post/create", methods=['POST'])
@@ -16,6 +18,7 @@ def create_post_task():
                     categories=l,
                     tags=request.values['tags'])
 
+    flash("Post Saved")
     return jsonify(result=a)
 
 
@@ -24,7 +27,8 @@ def create_post_task():
 def update_post_task():
     l = request.values.getlist('categories')
     l.pop(0)
-    a = update_post(title=request.values['title'],
+    a = update_post(request.values['postid'],
+                    title=request.values['title'],
                     content=request.values['content'],
                     decription=request.values['description'],
                     categories=l,
@@ -35,5 +39,17 @@ def update_post_task():
 @app.route("/blogon/tasks/post/publish", methods=['POST'])
 @blogon_login_required
 def publish_post_task():
-    publish_post(request.form['postid'])
+    a = publish_post(request.form['postid'])
     flash("Your post has been published.")
+    return redirect(url_for('posts_page'))
+
+
+@app.route("/blogon/tasks/post/delete", methods=['POST'])
+@blogon_login_required
+def delete_post_task():
+    a = delete_post(request.values['postid'])
+    if a == -1:
+        flash("You are not the author of this post")
+    else:
+        flash("Post has been deleted")
+    return redirect(url_for('posts_page'))
