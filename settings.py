d0d12dc5111723bb2548ad77d9d94ec04d409e02
Sahil Shah <shahsahil007@gmail.com>
2017-04-11 22:53:23 +0530

    Added about page

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 from main import app
-from flask import render_template, request, redirect, url_for, session, flash
+from flask import render_template, request, redirect, url_for, session, flash, jsonify
 from actions import connect,close
 
-@app.route('/update_settings', methods=['POST'])
-def update_settings():
+@app.route('/update_name', methods=['POST'])
+def update_name():
 	name = request.json['name']
+	about = request.json['about']
 	c, conn = connect()	
 	c.execute("SELECT * FROM settings WHERE setting='name'")
 	val = c.fetchone();
@@ -12,16 +13,33 @@ def update_settings():
 		c.execute("INSERT INTO settings(setting, value) values(%s,%s)",('name',str(name)))
 	else:
 		c.execute("UPDATE settings SET value = %s WHERE setting = %s", (str(name), 'name'))
+	c.execute("UPDATE settings SET value = %s WHERE setting = %s", (str(about), 'about'))
 	#c.execute("IF EXISTS (SELECT * FROM settings WHERE setting='name') BEGIN UPDATE settings SET value = %s WHERE setting = 'name' END ELSE BEGIN INSERT INTO settings(setting, value) values('name',%s) END",(name,name))
 	conn.commit()
 	
 	close(c, conn)
 	return "SUCCESS"
 	
-def get_settings():
+def get_name():
 	c, conn = connect()	
 	c.execute("SELECT value FROM settings WHERE setting='name'")
 	val = c.fetchone();
 	print (val)
 	close(c, conn)
 	return val
+
+	
+@app.route("/get_settings", methods=['POST'])
+def get_settings():
+	values = fetch_settings()
+	return jsonify(values)
+	
+def fetch_settings():
+	c, conn = connect()	
+	c.execute("SELECT value FROM settings WHERE setting='name'")
+	val = c.fetchone();
+	c.execute("SELECT value FROM settings WHERE setting='about'")
+	val2 = c.fetchone();
+	values = {'name':val['value'], 'about':val2['value']}
+	close(c, conn)
+	return values
